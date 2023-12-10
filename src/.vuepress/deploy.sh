d67249917643bd1ea33f05f36cf504b7e50c59d5
svvk.cn <ceo@svvk.cn>
2023-12-11 03:07:37 +0800

    1

--- src/.vuepress/deploy.sh
+++ src/.vuepress/deploy.sh
@@ -13,7 +13,14 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 请把 svvkcn/my-docs.git 换成你的项目地址
+git push -f git@github.com:svvkcn/my-docs.git master:gh-pages
+
 # 这里是填写你的github仓库地址，把git@github.com:xxx/xxx.git替换成你的仓库地址
-git push -f https://github.com/svvkcn/my-docs.git master:gh-pages
+#git push -f https://github.com/svvkcn/my-docs.git master:gh-pages
 
 cd -
